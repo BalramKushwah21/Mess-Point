3d81d2a9b31d9fed3675529664e356c3470abc60
Balram Kushwah <bkushwah1081@gmail.com>
2026-06-20 11:05:29 +0530

    Trying to solve verifying loop

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
 	// FIX: Localhost aur Vercel (HTTPS) dono ki cookies ko check karein
 	const token =
 		request.cookies.get("next-auth.session-token") ||
